decb9556d9614bdbb87872b32ec4198390c243e5
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-05-26 18:43:34 +0100

    a

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,24 +25,24 @@ function Home() {
                                             </p>
                                         </div>
                                     </div>
-                                </Slide>
-                                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                                    <div className="rounded-md shadow">
-                                        <Zoom>
+                                    <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                                        <div className="rounded-md shadow">
+
                                             <Link
-                                                to="/contact"
+                                                to="/about"
                                                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                                             >
-                                                Contact Me
+                                                About ME
                                             </Link>
-                                        </Zoom>
+
+                                        </div>
                                     </div>
-                                </div>
+                                </Slide>
                             </div>
                         </main>
                     </div>
                 </div>
-             
+
             </div>
 
             {/* Projects Section */}
