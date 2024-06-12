d17c2902e0eb537df26631caec10e33487a1049e
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-06-12 21:19:10 +0100

    new port folio

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import About from "./About/About";
-import Contact from "./Contact/Contact";
-import Home from "./Home/Home";
-import Project from "./Project/Project";
-import Skilles from './Skilles/Skilles';
-import More from './More/More';
-export {About, Contact, Home, Project , Skilles , More}
