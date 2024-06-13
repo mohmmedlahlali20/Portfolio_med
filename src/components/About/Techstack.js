7844d1afec13250c075c3fee562d0980ac5f438c
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2024-06-13 10:06:30 +0100

    skilzat

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 // import { SiCakephp } from "react-icons/cg";
 
@@ -27,50 +27,42 @@ import {
 import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
+  const [hoveredSkill, setHoveredSkill] = useState("");
+
+  const skills = [
+    { Icon: SiPhpmyadmin, name: "PhpMyAdmin" },
+    { Icon: DiJavascript1, name: "JavaScript" },
+    { Icon: SiPhp, name: "PHP" },
+    { Icon: DiNodejs, name: "Node.js" },
+    { Icon: DiReact, name: "React" },
+    { Icon: SiLaravel, name: "Laravel" },
+    { Icon: SiMysql, name: "MySQL" },
+    { Icon: SiExpress, name: "Express" },
+    { Icon: DiGit, name: "Git" },
+    { Icon: SiBootstrap, name: "Bootstrap" },
+    { Icon: SiTailwindcss, name: "Tailwind CSS" },
+    { Icon: SiPostgresql, name: "PostgreSQL" },
+    { Icon: SiDocker, name: "Docker" }
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPhpmyadmin />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPhp />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLaravel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiExpress />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiBootstrap />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTailwindcss />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiTrello />
-      </Col> */}
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
+      {skills.map(({ Icon, name }) => (
+        <Col
+          key={name}
+          xs={4}
+          md={2}
+          className="tech-icons"
+          onMouseEnter={() => setHoveredSkill(name)}
+          onMouseLeave={() => setHoveredSkill("")}
+          style={{ position: "relative", textAlign: "center" }}
+        >
+          {hoveredSkill === name && (
+            <div className="skill-name">{name}</div>
+          )}
+          <Icon />
+        </Col>
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
@@ -8,24 +8,37 @@ import {
   SiUbuntu,
 } from "react-icons/si";
 
+import './About.css'
+
 function Toolstack() {
+  const [hoveredTool, setHoveredTool] = useState("");
+
+  const tools = [
+    { Icon: SiUbuntu, name: "Ubuntu" },
+    { Icon: SiVisualstudiocode, name: "Visual Studio Code" },
+    { Icon: SiPostman, name: "Postman" },
+    { Icon: SiTrello, name: "Trello" },
+    { Icon: SiVercel, name: "Vercel" },
+  ];
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiUbuntu />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTrello />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
+      {tools.map(({ Icon, name }) => (
+        <Col
+          key={name}
+          xs={4}
+          md={2}
+          className="tech-icons"
+          onMouseEnter={() => setHoveredTool(name)}
+          onMouseLeave={() => setHoveredTool("")}
+          style={{ position: "relative", textAlign: "start" }}
+        >
+          {hoveredTool === name && (
+            <div className="skill-name">{name}</div>
+          )}
+          <Icon />
+        </Col>
+      ))}
     </Row>
   );
 }
